9278e52b5c7acb3c658fe255092788a45b8e7806
Fletcher Watson <fbwthe6@gmail.com>
2016-07-27 10:08:29 -0500

    completed step 4(5 and 6)

--- predator.js
+++ predator.js
@@ -2,4 +2,26 @@ var predator = (function () {
   var _carnivores = []
   var _herbivores = []
 
+  return {
+    getCarnivores: function (callback) {
+      var xhr = XMLHTttpRequest()
+      xhr.open('GET', 'carnivores.json')
+      xhr.addEventListener('load', function() {
+        carnivores = JSON.parse(xhr.responseText)
+        callback(carnivores)
+      })
+      xhr.send()
+
+
+    },
+    getHerbivores: function (callback) {
+      var xhr = XMLHTttpRequest()
+      xhr.open('GET', 'herbivores.json')
+      xhr.addEventListener('load', function() {
+        herbivores = JSON.parse(xhr.responseText)
+        callback(herbivores)
+      })
+      xhr.send()
+  }
+
 }())
